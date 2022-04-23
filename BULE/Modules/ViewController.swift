66e3dcfc6fdda14f5254f3b7648b706c8fb97478
Charlie Walton <103379122+kwozgo@users.noreply.github.com>
2022-04-23 18:22:07 +0300

    Added target value which generates for every round

--- BULE/Modules/ViewController.swift
+++ BULE/Modules/ViewController.swift
@@ -9,15 +9,18 @@ import UIKit
 
 final class ViewController: UIViewController {
     
-    var currentValue: Int = 50
+    @IBOutlet private var slider: UISlider!
+    
+    private var currentValue: Int = 0
+    private var targetValue: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.startRound()
     }
     
     @IBAction private func hitMePressed() {
-        let message = "The value of the slider is \(self.currentValue)"
+        let message = "The value of the slider is \(self.currentValue)" + "\nThe target value is \(self.targetValue)"
         let alert = UIAlertController(
             title: "Hello, World",
             message: message,
@@ -28,10 +31,17 @@ final class ViewController: UIViewController {
             style: .default
         )
         alert.addAction(action)
-        present(alert, animated: true)
+        self.present(alert, animated: true)
+        self.startRound()
     }
     
     @IBAction private func sliderMoved(_ slider: UISlider) {
         self.currentValue = lroundf(slider.value)
+    }
+    
+    private func startRound() {
+        self.targetValue = .random(in: 1...100)
+        self.currentValue = 50
+        self.slider.value = Float(self.currentValue)
     }
 }
